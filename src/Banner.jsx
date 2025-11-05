fb0eb573a81b526d8bc054280d4aa9a4b437a1e2
davide-saietti <imbianchino.nando@gmail.com>
2025-11-05 15:35:10 +0100

    feat: banner component

---
+++ src/Banner.jsx
@@ -0,0 +1,9 @@
+export default function Banner(){
+    return(
+        <ul className="nav bg-light py-3">
+
+    <h2 className="text-color-dark px-4" aria-current="page" href="#">Learn Web Development</h2>
+
+</ul>
+    )
+}
